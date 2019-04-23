a660eadcec25e819e459e6f159b8e2e378822f70
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2019-04-23 16:26:12 +0200

    Resolve only commands from root `/commands` dir

--- commands/test.js
+++ commands/test.js
@@ -1,13 +1,13 @@
-import assert from '../assert.js'
-import html from '../../shared/nanohtml-v1.2.4.js'
-import joinPath from '../join-path.js'
+import assert from '../modules/assert.js'
+import html from '../shared/nanohtml-v1.2.4.js'
+import joinPath from '../modules/join-path.js'
 
 export default async function (opts, module) {
   var t = new Test()
   var path = joinPath(window.location.pathname, module)
   var mod = await import(`dat:/${path}?nocache=${Date.now()}`)
   assert(typeof mod.test === 'function')
-  
+
   try {
     console.time(module + ' tests')
     await mod.test(t)
@@ -17,7 +17,7 @@ export default async function (opts, module) {
   } finally {
     console.timeEnd(module + ' tests')
   }
-  
+
   if (!t.passed) {
     return html`<div class="error error-stack">
       Tests failed, check console for details
@@ -29,7 +29,7 @@ class Test {
   constructor () {
     this.passed = true
   }
-  
+
   ok (condition, msg) {
     if (condition) {
       console.info(msg)
--- modules/dterm-load-command.js
+++ modules/dterm-load-command.js
@@ -1,24 +1,32 @@
 import getEnv from '../modules/dterm-env.js'
+import joinPath from '../modules/join-path.js'
+import parsePath from '../modules/dterm-parse-path.js'
 
 export default async function (cmd, location) {
-  var installed = (await getEnv()).commands[cmd]
-  return installed ? import(installed) : recurseDirs(cmd, location)
+  return import(await findCommand(cmd, location))
 }
 
-async function recurseDirs (cmd, location) {
-  var isRoot = location === '/' || location === ''
-  var url = isRoot
-    ? new URL(import.meta.url).origin
-    : 'dat:/' + location
+async function findCommand (cmd, location) {
+  var installed = (await getEnv()).commands[cmd]
+  return installed || findInArchive(cmd, location)
+}
+
+async function findInArchive (cmd, location) {
+  var path, stat, cwd = parsePath(location)
 
   try {
-    return await import(`${url}/commands/${cmd}.js`)
+    path = joinPath('commands', cmd + '.js')
+    stat = await cwd.archive.stat(path)
   } catch (err) {
-    if (isRoot) {
-      throw new Error(cmd + ': command not found')
-    } else {
-      var parent = location.split('/').slice(0, -1).join('/')
-      return recurseDirs(cmd, parent)
-    }
+    throw new Error(getError(cmd))
   }
+  if (stat.isDirectory()) {
+    throw new Error(getError(cmd))
+  }
+
+  return `dat://${cwd.key}/${path}`
+}
+
+function getError (cmd) {
+  return cmd + ': command not found'
 }
