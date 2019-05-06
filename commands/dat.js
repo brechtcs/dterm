6fb5ccebe362209ee8f4c3a1eac05adaf32c1aab
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2019-05-06 08:36:38 +0200

    Add `dat fork` command

--- commands/dat.js
+++ commands/dat.js
@@ -1,6 +1,6 @@
 import cd from './cd.js'
 import html from '../vendor/nanohtml-v1.2.4.js'
-import parsePath from '../modules/dterm-parse-path.js'
+import publicState from '../modules/dterm-public-state.js'
 import shortenHash from '../modules/shorten-hash.js'
 
 export async function create (opts) {
@@ -8,9 +8,19 @@ export async function create (opts) {
   return cd(null, dat.url)
 }
 
+export async function fork (opts, url) {
+  if (!url) {
+    let cwd = publicState.cwd
+    let info = await cwd.archive.getInfo()
+    url = 'dat://' + info.key
+  }
+  let fork = await DatArchive.fork(url)
+  return cd(null, fork.url)
+}
+
 export async function add (opts, url) {
   if (!url) {
-    let cwd = parsePath(window.location.pathname)
+    let cwd = publicState.cwd
     let info = await cwd.archive.getInfo()
     url = 'dat://' + info.key
   }
